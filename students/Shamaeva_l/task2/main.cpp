3efa948e816d5be069419db852a69eccad3aaa7e
Shamaevaa <Shamaeva.lizun4ik@yandex.ru>
2018-03-03 18:41:56 +0400

    Второе задание!

--- students/Shamaeva_l/task2/main.cpp
+++ students/Shamaeva_l/task2/main.cpp
@@ -1,6 +1,6 @@
-#include <iostream>
-#include <string>
-#include <conio.h>
+#include <iostream> 
+#include <string> 
+#include <conio.h> 
 
 using namespace std;
 
@@ -11,17 +11,17 @@ private:
 	int ind;
 	char ch;
 public:
-	String_() //Конструктор                                                          
+	String_() //Конструктор 
 	{
 		strings = " ";
 	}
 
-	~String_() //Деструктор                                                       
+	~String_() //Деструктор 
 	{
-		//delete[] strings;
+		//delete[] strings; 
 	}
 
-	void SetStr() //Введение новой строки                                             
+	void SetStr() //Введение новой строки 
 	{
 		cout << "Vvedite stroku: ";
 		cin >> strings;
@@ -35,19 +35,19 @@ public:
 		}
 	}
 
-	void LenStr() //Длина строки
+	void LenStr() //Длина строки 
 	{
 		cout << "Dlina stroki: " << strings.size() << endl;
 	}
 
-	void IndStr() //Символ по индексу
+	void IndStr() //Символ по индексу 
 	{
 		cout << "Vvedite indeks: ";
 		cin >> ind;
 		cout << "Simvol: " << strings[ind - 1] << endl;
 	}
 
-	void IndRepStr() //Замена символа по индексу
+	void IndRepStr() //Замена символа по индексу 
 	{
 		cout << "Vvedite indeks: ";
 		cin >> ind;
@@ -57,43 +57,52 @@ public:
 		cout << " New Stroka: " << strings << endl;
 	}
 
-	bool PoliStr() //Проверка на полиндром
+
+	bool PoliStr() //Проверка на полиндром 
 	{
 		for (int i = 0; i < strings.size() / 2; ++i)
 		{
 			if (strings[i] != strings[strings.size() - i - 1])
 			{
-				//cout << "Stroka ne polindrom! \n";
+				//cout « "Stroka ne polindrom! \n"; 
 				return false;
 			}
 		}
 		return true;
 	}
-	void LatStr() // Нахождение символов латинского алфавита
-	{
-		int a = 1;
-		int b;
-		int i, j;
-		for (i = 1; i < strings.size(); i++);
-		{
-			b = 0;
-			for (j = 0; j < i; j++);
-			{
-				if (strings[i] != strings[j])
-					b++;
-				else;
-			}
-			if (b == i)
-				a++;
-		}
-		cout << a << endl;
 
+	void subStr(int fp, int sp) // Выделение подстроки
+	{
+		int i;
+		for (int i = fp-1; i < fp+sp-1; i++)
+		{
+			cout« strings[i];
+		}
+
+	}
+	void LatStr() // Нахождение символов латинского алфавита 
+	{
+		int count = 0;
+		bool flag = false;
+		for (int i = 0; i < strings.size(); i++) {
+			for (int j = 0; j < strings.size(); j++) {
+				if (i != j)
+				{
+					if (strings[i] == strings[j]) flag = true;
+				}
+			}
+			if (flag == false)
+				count++;
+			flag = false;
+		}
+		cout << "Simvolov : " << count « endl;
 	}
 
 };
 
 int main()
 {
+	int fp, sp;
 	String_ obj1;
 	obj1.SetStr();
 	obj1.LenStr();
@@ -101,6 +110,12 @@ int main()
 	obj1.PoliStr();
 	obj1.IndRepStr();
 	obj1.LatStr();
+	obj1.PoliStr();
+	cout << "VVedite kollicestvo: ";
+	cin >> sp;
+	cout << "VVedite indeks: ";
+	cin >> fp;
+	obj1.subStr(fp, sp);
 	if (obj1.PoliStr())
 	{
 		cout << "Stroka polindrom.";
@@ -113,4 +128,3 @@ int main()
 	return 0;
 
 }
-
